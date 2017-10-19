e245ab7b3777b371fea84002a22d68ac64051d45
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2017-10-19 10:26:06 -0400

    Account for different ordering of boost::multi_array and numpy

--- zwatershed/zwatershed_main.cpp
+++ zwatershed/zwatershed_main.cpp
@@ -95,8 +95,6 @@ void merge_no_stats(int dimX, int dimY, int dimZ,
     for (size_t z = 0; z < dimZ; z++)
 	for (size_t y = 0; y < dimY; y++)
 	    for (size_t x = 0; x < dimX; x++)
-		seg_ptr[(z * dimY + y) * dimX + x] =\
+		seg_ptr[(z * dimY + y) * dimX + x] =
 		    result.seg_ref->data()[(x * dimY + y) * dimZ + z];
- }
-
-
+}
